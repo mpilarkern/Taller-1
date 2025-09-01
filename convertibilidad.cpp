da11adc94c07ffae653ecf955b6123d2b9acbe44
pilarkern <pilikern04@gmail.com>
2025-09-01 14:00:45 -0300

    más dudas y primera implementación de convertibilidad en c++

--- convertibilidad.cpp
+++ convertibilidad.cpp
@@ -2,10 +2,30 @@
 
 using namespace std;
 
-int convertibilidad(int x, int y){
-    return 0;
+bool convertibilidad(int x, int y){
+    if (x == y) {
+        return true;
+    } else if (2*x <= y or 10*x + 1 <= y) {
+        return convertibilidad(2*x, y) or convertibilidad(10*x + 1, y);
+    } else {
+        return false;
+    }
 }
 
 int main(){
+    cout << boolalpha;
+
+    cout << convertibilidad(1, 82) << endl; //True
+    cout << convertibilidad(1, 2) << endl; //True
+    cout << convertibilidad(1, 11) << endl; //True
+    cout << convertibilidad(2, 162) << endl; //True
+    cout << convertibilidad(5, 101) << endl; //True
+    cout << convertibilidad(2, 21) << endl; //True
+    cout << convertibilidad(53, 53) << endl; //True
+    cout << convertibilidad(2, 22) << endl; //False
+    cout << convertibilidad(2, 45) << endl; //False
+    cout << convertibilidad(3, 10) << endl; //False
+    cout << convertibilidad(2, 3) << endl; //False
+
     return 0;
 }
--- prueba.py
+++ prueba.py
@@ -22,4 +22,5 @@ print(convertibilidad(2, 3)) #False
 1. x e y son siempre números enteros?
 2. la secuencia de transformaciones se imprime como array o como números separados?
 3. cuando lo paso a c++ de qué tipo tiene que ser la salida? (o sea debería ser int convertibilidad(x, y)?)
+4. cuando dice imprimir tengo que poner un cout o está bien un return?
 """
