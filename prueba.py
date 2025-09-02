39347a9d50be7dc02470c4b4092bce2b79fc7c3e
pilarkern <pilikern04@gmail.com>
2025-09-02 10:20:37 -0300

    agrego duda

--- prueba.py
+++ prueba.py
@@ -23,4 +23,5 @@ print(convertibilidad(2, 3)) #False
 2. la secuencia de transformaciones se imprime como array o como números separados?
 3. cuando lo paso a c++ de qué tipo tiene que ser la salida? (o sea debería ser int convertibilidad(x, y)?)
 4. cuando dice imprimir tengo que poner un cout o está bien un return?
+5. cómo es el tema del main?
 """
