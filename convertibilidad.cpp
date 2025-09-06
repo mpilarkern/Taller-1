814f29ad1e654000eb06f994d96525600b9b55b4
pilarkern <pilikern04@gmail.com>
2025-09-06 14:01:46 -0300

    convertibilidad terminado

--- convertibilidad.cpp
+++ convertibilidad.cpp
@@ -1,38 +1,41 @@
 #include<iostream>
 #include<vector>
-
+ 
 using namespace std;
-
+ 
 bool convertibilidad(int x, int y, vector<int>& transformaciones){
+    transformaciones.push_back(y);
     if (x == y) {
-        transformaciones.push_back(x);
         return true;
-    } else if (2*x <= y && convertibilidad(2*x, y, transformaciones)) {
-        transformaciones.push_back(x);
-        return true;
-    } else if (10*x + 1 <= y && convertibilidad(10*x + 1, y, transformaciones)) {
-        transformaciones.push_back(x);
-        return true;
-    } else {
+    }
+    if (x > y) {
         return false;
     }
-}
 
+    if (y % 2 == 0) {
+        return convertibilidad(x, y/2, transformaciones);
+     } else if (y % 10 == 1) {
+        return convertibilidad(x, (y - 1)/10, transformaciones);
+     } else {
+        return false;
+     }
+}
+ 
 int main(){
     int x, y;
     vector<int> transformaciones;
-
+ 
     cin >> x >> y;
-
+ 
     if (convertibilidad(x, y, transformaciones)) {
         cout << "YES" << "\n";
         cout << transformaciones.size() << "\n";
-        for (int i = transformaciones.size() - 1; i >= 0; i--){
+        for (int i = transformaciones.size() - 1; i >= 0 ; i--){
             cout << transformaciones[i] << " ";
         }
     } else {
         cout << "NO";
     }
-
+ 
     return 0;
 }
